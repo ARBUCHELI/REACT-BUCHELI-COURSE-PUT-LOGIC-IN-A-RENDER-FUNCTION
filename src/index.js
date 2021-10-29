060af32752bd586635e276c01ac740b5cd46c8d9
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-10-29 09:53:51 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg"
+  },
+  {
+    title: "Hey Guys!  Wait Up!",
+    src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg"
+  },
+  {
+    title: "Yikes",
+    src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg"
+  }
+];
+
+// New component class starts here:
+class Friend extends React.Component {
+  render() {
+    const friend = friends[1];
+    return (
+      <div>
+        <h1>
+          {friend.title}
+        </h1>
+        <img
+          src={friend.src} alt=''
+        />
+      </div>
+    );
+  };
+};
+
+ReactDOM.render(<Friend />, document.getElementById('app'))
